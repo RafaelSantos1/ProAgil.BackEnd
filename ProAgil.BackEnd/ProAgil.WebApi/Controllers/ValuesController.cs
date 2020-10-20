bd0c94fd77c196f69dd32f1de35601773b28ad49
rafaelsantos1 <rafael_oliveira034@hotmail.com>
2020-10-20 15:31:46 -0300

    criação do repositório

--- ProAgil.BackEnd/ProAgil.WebApi/Controllers/ValuesController.cs
+++ ProAgil.BackEnd/ProAgil.WebApi/Controllers/ValuesController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ProAgil.WebApi.Data;
-using ProAgil.WebApi.Model;
+using ProAgil.Repository;
+
 
 namespace ProAgil.WebApi.Controllers
 {
@@ -14,8 +14,8 @@ namespace ProAgil.WebApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        public readonly DataContext _context;
-        public ValuesController(DataContext context)
+        public readonly ProAgilContext _context;
+        public ValuesController(ProAgilContext context)
         {
             _context = context;
         }
@@ -42,7 +42,7 @@ namespace ProAgil.WebApi.Controllers
         {            
             try
             {
-                var resultado = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                var resultado = await _context.Eventos.FirstOrDefaultAsync(x => x.Id == id);
                 return Ok(resultado);
             }
             catch (System.Exception)
